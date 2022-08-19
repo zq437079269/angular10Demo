ef170a0a02004d57abf6b6700d70ed0aba623414
zq437079269 <437079269@qq.com>
2022-08-19 13:32:51 +0800

    2022-08019

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgModel} from "@angular/forms";
+import {TransferItem} from "./components/transfer-panel/types";
 
 @Component({
   selector: 'app-root',
+  // template:`<h1 class="title">{{title}}</h1>
+  // <p>{{name}}</p>
+  // `,
+  // styles:[`
+  //
+  // `]
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular10';
+  showModal=false
+  size=16
+  list:TransferItem[]=[];
+  //private 私有变量 只在内部使用
+  //可调取ngModel内置方法及属性
+  @ViewChild(NgModel) private ngModel:any;
+//   name='我丢！ ';
+//   price=30;
+//   gender=0
+//   //ES6语法，在类里 get方法使方法变为变量 可直接使用
+//   get job():string{
+//     return  '哈哈哈'
+//   }
+//   getVal():number{
+//     return 33;
+// }
+  madaoSrc='../assets/madao.jpg'
+  customTitle = 'bbb';
+  user2={
+    name:'madao',
+    pic:'../assets/madao.jpg'
+  }
+  isPrimary=true
+  btnCls='btn btn-primary'
+  btnCls2=['btn','btn-info']
+  btnCls3={
+     btn:true,
+    'btn-secondary':true
+  }
+
+  style1 = 'width: 200px;height: 50px;text-align: center;border: 1px solid;';
+  style2 = ['width', '200px', 'height', '50px', 'text-align', 'center', 'border', '1px solid']; // 有问题
+  style3 = {
+    color:'red',
+    width: '200px',
+    height: '50px',
+    'text-align': 'center',
+    border: '1px solid',
+    backgroundColor:'#ff7632'  //background-color也可以
+  };
+  onClose(){
+    this.showModal=false
+  }
+  //点击确定后续操作的方法
+  confirm(){
+    console.log('点击了确定')
+  }
+  // changes(e:number){
+  //  this.size=e
+  // }
+  constructor(
+  ) {
+    this.setList()
+  }
+
+ private  setList(){
+    this.list = [];
+    const prefix = 'item' + Date.now().toString().slice(-3);
+    for (let i = 0; i < 20; i++) {
+      this.list.push({
+        key: prefix + '_' + i,
+        value: `${prefix}${i + 1}`,
+        checked: i % 6 === 0,
+      });
+    }
+  }
+  resect(){
+    this.setList()
+  }
+  onchange(selecteds:TransferItem[]){
+    console.log(selecteds)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { TextComponent } from './demos/text/text.component';
+import { DialogComponent } from './components/dialog/dialog.component';
+import { PseudoClassComponent } from './demos/pseudo-class/pseudo-class.component';
+import { SizerComponent } from './components/sizer/sizer.component';
+import { NgmoduleComponent } from './components/ngmodule/ngmodule.component';
+import { IfComponent } from './components/if/if.component';
+import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
+import { NgNgForComponent } from './components/ng-ng-for/ng-ng-for.component';
+import { TplComponent } from './components/tpl/tpl.component';
+import { TplOperatorsComponent } from './components/tpl-operators/tpl-operators.component';
+import { TransferPanelComponent } from './components/transfer-panel/transfer-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextComponent,
+    DialogComponent,
+    PseudoClassComponent,
+    SizerComponent,
+    NgmoduleComponent,
+    IfComponent,
+    NgSwitchComponent,
+    NgNgForComponent,
+    TplComponent,
+    TplOperatorsComponent,
+    TransferPanelComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule, //内置了angular指令
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
